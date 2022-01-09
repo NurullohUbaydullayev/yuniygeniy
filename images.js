52ff38722008d70d9c34d6652cb3e83400c1801e
NurullohUbaydullayev <nurulloh.ubaydullayev2004@gmail.com>
2022-01-09 13:25:13 +0500

    Comment fetch

--- images.js
+++ images.js
@@ -1,21 +1,16 @@
-const ul = document.getElementById("ul");
-console.log(ul);
-fetch("https://yuniyapi.herokuapp.com/allImage")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
+// const ul = document.getElementById("ul");
+// fetch("https://yuniyapi.herokuapp.com/allImage")
+//   .then(res => res.json())
+//   .then(data => {
+//     data.forEach(e => {
+//       const img = document.createElement("img");
+//       img.style.width = "400px";
+//       img.src = `https://yuniyapi.herokuapp.com/` + e.imagename;
+//       //   img.dataset.imgId =
+//       const div = document.createElement("div");
+//       div.classList.add("swiper-slide");
+//       div.append(img);
 
-    data.forEach((e) => {
-      console.log(e);
-      const img = document.createElement("img");
-      img.style.width = "400px";
-      img.src = `https://yuniyapi.herokuapp.com/` + e.imagename;
-      console.log(e);
-      //   img.dataset.imgId =
-      const div = document.createElement("div");
-      div.classList.add("swiper-slide");
-      div.append(img);
-
-      ul.append(div);
-    });
-  });
+//       ul.append(div);
+//     });
+//   });
